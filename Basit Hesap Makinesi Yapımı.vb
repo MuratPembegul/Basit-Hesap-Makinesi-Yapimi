de91cfb5d41175654b9ae7a72ec9d94655ca9a90
Murat Pembegül <mpembegul15@gmail.com>
2023-01-06 10:50:04 +0200

    Update Basit Hesap Makinesi Yapımı.vb

--- Basit Hesap Makinesi Yapımı.vb
+++ Basit Hesap Makinesi Yapımı.vb
@@ -1,51 +1,35 @@
 Dim x As Single
 Dim y As Single
 Dim z As Double
-
-
 toplama kodu
-
 Private Sub Command1_Click()
 x = Val(Text1.Text)
 y = Val(Text2.Text)
 z = x + y
 Text3.Text = z
 End Sub
-
-
-
 çıkartma kodu
-
 Private Sub Command2_Click()
 x = Val(Text1.Text)
 y = Val(Text2.Text)
 z = x - y
 Text3.Text = z
 End Sub
-
-
 çarpma kodu
-
 Private Sub Command3_Click()
 x = Val(Text1.Text)
 y = Val(Text2.Text)
 z = x * y
 Text3.Text = z
 End Sub
-
-
 bölme kodu
-
 Private Sub Command4_Click()
 x = Val(Text1.Text)
 y = Val(Text2.Text)
 z = x / y
 Text3.Text = z
 End Sub
-
-
 temizle kodu
-
 Private Sub Command5_Click()
 Text1.Text = ""
 Text2.Text = ""
